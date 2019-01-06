0c75fd52c22fc3243c5c6df47e100d1f77734b88
Rdulemba <rdulemba@gmail.com>
2019-01-06 23:33:39 +0100

    GameSetUp Screen completed (until code refactoring)

--- RD_Colonization/Code/Screens/GameSetUpScreen.cs
+++ RD_Colonization/Code/Screens/GameSetUpScreen.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GeonBit.UI;
+using GeonBit.UI.Utils;
 using GeonBit.UI.Entities;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -17,8 +18,11 @@ namespace RD_Colonization.Code.Screens
     class GameSetUpScreen : DefaultScreen
     {
         private Texture2D portraits;
-        List<Entity> rootEntities = new List<Entity>(); 
+        List<Entity> rootEntities = new List<Entity>();
+        List<int> sizes = new List<int> { 30, 40, 50, 60 };
         Dictionary<string, List<Entity>> pairsOfElements = new Dictionary<string, List<Entity>>();
+        private string civilizationKey = "Random";
+        private string sizeKey = "Random";
 
         public GameSetUpScreen(ColonizationGame game) : base(game)
         {
@@ -49,7 +53,16 @@ namespace RD_Colonization.Code.Screens
             Button startGameButton = new Button("Start", ButtonSkin.Default, Anchor.BottomRight, new Vector2(200, 50));
             startGameButton.OnClick += (Entity entity) =>
             {
-
+                MessageBox.ShowMsgBox("Start game", String.Format("Your current settings are: \nCivilization{1}\nSize: {1}",civilizationKey, sizeKey), new MessageBox.MsgBoxOption[] {
+                                new MessageBox.MsgBoxOption("Cancel", () => 
+                                {
+                                    return true;
+                                }),
+                                new MessageBox.MsgBoxOption("Confirm", () => 
+                                {
+                                    ScreenManager.setScreen(gameScreenString);
+                                    return true; })
+                                });
             };
 
             Panel mainPanel = new Panel(new Vector2(800, 450), PanelSkin.None, Anchor.TopCenter, new Vector2(10, 10));
@@ -58,42 +71,71 @@ namespace RD_Colonization.Code.Screens
             tabs.Padding = new Vector2(10, 10);
 
             TabData countryTab = tabs.AddTab("Country");
-            Panel listPanel = new Panel(new Vector2(300, tabs.Size.Y), anchor: Anchor.AutoInline);
-            Panel descriptionPanel = new Panel(new Vector2(400, tabs.Size.Y), anchor: Anchor.AutoInline);
-            countryTab.panel.AddChild(listPanel);
-            listPanel.AddChild(new Header("List:"));
-            listPanel.AddChild(new HorizontalLine());
-            foreach (String s in CivilizationManager.getNames())
             {
-                RadioButton r = new RadioButton(s);
-                r.Identifier = s;
-                r.OnClick += (Entity entity) =>
+                Panel listPanel = new Panel(new Vector2(300, tabs.Size.Y), anchor: Anchor.AutoInline);
+                Panel descriptionPanel = new Panel(new Vector2(400, tabs.Size.Y), anchor: Anchor.AutoInline);
+                countryTab.panel.AddChild(listPanel);
+                listPanel.AddChild(new Header("List:"));
+                listPanel.AddChild(new HorizontalLine());
+                RadioButton randomCivilization = new RadioButton("Random", isChecked: true);
+                randomCivilization.OnClick += (Entity entity) =>
                 {
                     foreach (KeyValuePair<string, List<Entity>> item in pairsOfElements)
                         foreach (Entity e in item.Value)
                             e.Visible = false;
-                    List<Entity> tmp = null;
-                    pairsOfElements.TryGetValue(s, out tmp);
-                    tmp[0].Visible = true;
-                    tmp[1].Visible = true;
+                    civilizationKey = "Random";
                 };
-                listPanel.AddChild(r);
-            }
-            countryTab.panel.AddChild(descriptionPanel);
-            descriptionPanel.AddChild(new Header("Description"));
-            descriptionPanel.AddChild(new HorizontalLine());
-            foreach (String s in CivilizationManager.getNames())
-            {
-                Image i = CivilizationManager.getPortrait(s);
-                Paragraph p = new Paragraph(CivilizationManager.getInformations(s));
-                descriptionPanel.AddChild(i);
-                descriptionPanel.AddChild(p);
-                i.Visible = false;
-                p.Visible = false;
-                pairsOfElements.Add(s, new List<Entity> { i, p });
+                 listPanel.AddChild(randomCivilization);
+                foreach (String s in CivilizationManager.getNames())
+                {
+                    RadioButton r = new RadioButton(s);
+                    r.Identifier = s;
+                    r.OnClick += (Entity entity) =>
+                    {
+                        foreach (KeyValuePair<string, List<Entity>> item in pairsOfElements)
+                            foreach (Entity e in item.Value)
+                                e.Visible = false;
+                        List<Entity> tmp = null;
+                        pairsOfElements.TryGetValue(s, out tmp);
+                        tmp[0].Visible = true;
+                        tmp[1].Visible = true;
+                        civilizationKey = r.Identifier;
+                    };
+                    listPanel.AddChild(r);
+                }
+                countryTab.panel.AddChild(descriptionPanel);
+                descriptionPanel.AddChild(new Header("Description"));
+                descriptionPanel.AddChild(new HorizontalLine());
+                foreach (String s in CivilizationManager.getNames())
+                {
+                    Image i = CivilizationManager.getPortrait(s);
+                    Paragraph p = new Paragraph(CivilizationManager.getInformations(s));
+                    descriptionPanel.AddChild(i);
+                    descriptionPanel.AddChild(p);
+                    i.Visible = false;
+                    p.Visible = false;
+                    pairsOfElements.Add(s, new List<Entity> { i, p });
+                }
             }
 
             TabData sizeMapTab = tabs.AddTab("Map");
+            {
+                RadioButton randomSize = new RadioButton("Random", isChecked: true);
+                randomSize.OnClick += (Entity entity) =>
+                {
+                    sizeKey = "Random";
+                };
+                sizeMapTab.panel.AddChild(randomSize);
+                foreach (int i in sizes)
+                {
+                    RadioButton r = new RadioButton(String.Format("{0}x{0}", i));
+                    r.OnClick += (Entity entity) =>
+                    {
+                        sizeKey = i.ToString();
+                    };
+                    sizeMapTab.panel.AddChild(r);
+                }
+            }
 
             UserInterface.Active.AddEntity(mainPanel);
             mainPanel.AddChild(tabs);
--- RD_Colonization/Code/Systems/MapDrawer.cs
+++ RD_Colonization/Code/Systems/MapDrawer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RD_Colonization.Code.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace RD_Colonization.Code
 {
     class MapDrawer
     {
+
+        private MapData mapData = null;
     }
 }
