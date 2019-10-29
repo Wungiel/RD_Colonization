5b67902418a4ebe57f8f5828f8ad09048f360b11
Rdulemba <rdulemba@gmail.com>
2019-10-29 22:04:51 +0100

    DDA managers added, packages imported

---
+++ RD_Colonization/Code/Managers/DDAEvolutionaryAIManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeneticSharp.Domain.Crossovers;
+using GeneticSharp.Domain.Fitnesses;
+using GeneticSharp.Domain.Mutations;
+using GeneticSharp.Domain.Populations;
+using GeneticSharp.Domain.Selections;
+using GeneticSharp.Domain.Terminations;
+
+
+namespace RD_Colonization.Code.Managers
+{
+    public class DDAEvolutionaryAIManager : BaseManager<DDAEvolutionaryAIManager>
+    {
+    }
+}
